984a4b4e42e1318cc903592f32ff2e3d7b5c7e6b
Sanoop Jose <sanoop.jose@honeywell.com>
2017-09-16 17:12:12 +0530

    Firebase integration with authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  user = null;
+  firebaseObj: FirebaseObjectObservable<any>;
+
+  constructor(
+    private auth: AuthService,
+    public db: AngularFireDatabase) { }
+  
+  ngOnInit() {
+    this.auth.getAuthState().subscribe(
+      (user) => this.user = user ? user.displayName : null);
+      console.log('User: ', this.user);
+  }
+
+  loginWithGoogle() {
+    this.auth.loginWithGoogle();
+  }
+
+  testDB() {
+    this.firebaseObj = this.db.object('/adminDetails');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AuthService } from './auth.service';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
